4e70dde736820a0053a2e4e452ac68c0e992404d
Barry Nouwt <barry.nouwt@tno.nl>
2020-12-01 12:11:53 +0100

    #2 make sure the graph pattern can contain a pattern

--- src/main/java/interconnect/ke/api/GraphPattern.java
+++ src/main/java/interconnect/ke/api/GraphPattern.java
@@ -2,4 +2,13 @@ package interconnect.ke.api;
 
 public class GraphPattern {
 
+	private String pattern;
+
+	public GraphPattern(String aPattern) {
+		this.pattern = aPattern;
+	}
+
+	public String getPattern() {
+		return this.pattern;
+	}
 }
