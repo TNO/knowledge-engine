644365b6c52eb8305ace65a03a0a1f3399c278bc
Barry Nouwt <barry.nouwt@tno.nl>
2021-01-05 15:58:26 +0100

    Introduce an interface that both the Matcher and Reasoner can use.

---
+++ src/main/java/interconnect/ke/sc/InteractionProcessor.java
@@ -0,0 +1,13 @@
+package interconnect.ke.sc;
+
+/**
+ * This interface is used to abstract away the details of the matching/reasoning
+ * process. Both the matcher and (in the future) the reasoner will implement
+ * this interface in a different way which results in different behaviour of the
+ * smart connector overall.
+ * 
+ * It receives 
+ */
+public interface InteractionProcessor {
+
+}
