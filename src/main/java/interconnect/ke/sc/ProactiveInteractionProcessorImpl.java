70f3b20ee5dab5f6a5f8d856b2ab28b356835052
Barry Nouwt <barry.nouwt@tno.nl>
2021-01-06 13:49:45 +0100

    Intermediate commit to get stuff from master.

--- src/main/java/interconnect/ke/sc/ProactiveInteractionProcessorImpl.java
+++ src/main/java/interconnect/ke/sc/ProactiveInteractionProcessorImpl.java
@@ -1,21 +1,47 @@
 package interconnect.ke.sc;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Future;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import interconnect.ke.api.AskResult;
-import interconnect.ke.api.PostResult;
 import interconnect.ke.api.RecipientSelector;
 import interconnect.ke.api.binding.BindingSet;
 import interconnect.ke.api.interaction.AskKnowledgeInteraction;
-import interconnect.ke.api.interaction.PostKnowledgeInteraction;
+import interconnect.ke.api.interaction.KnowledgeInteraction;
 
 public class ProactiveInteractionProcessorImpl implements ProactiveInteractionProcessor {
+
+	private static final Logger LOG = LoggerFactory.getLogger(ProactiveInteractionProcessorImpl.class);
+
+	private final Set<SingleInteractionProcessor> activeInteractionProcessors;
+	private final OtherKnowledgeBaseStore otherKnowledgeBaseStore;
 
 	@Override
 	public CompletableFuture<AskResult> processAsk(AskKnowledgeInteraction anAKI, RecipientSelector aSelector,
 			BindingSet aBindingSet) {
-		// TODO Auto-generated method stub
-		return null;
+
+		// in the MVP we interpret the recipient selector as a wildcard.
+		// retrieve other knowledge bases
+		List<OtherKnowledgeBase> otherKnowledgeBases = otherKnowledgeBaseStore.getOtherKnowledgeBases();
+		Set<KnowledgeInteraction> otherKnowledgeInteractions = new HashSet<KnowledgeInteraction>();
+		for (OtherKnowledgeBase otherKB : otherKnowledgeBases) {
+			otherKnowledgeInteractions.addAll(otherKB.getKnowledgeInteractions());
+		}
+
+		// create a new SingleInteractionProcessor to handle this ask.
+		SingleInteractionProcessor processor = new NaiveMatchingProcessor(otherKnowledgeInteractions,
+				this.messageReplyTracker);
+
+		activeInteractionProcessors.add(processor);
+
+		CompletableFuture<AskResult> future = new CompletableFuture<AskResult>();
+		return future;
 	}
 
 //	@Override
