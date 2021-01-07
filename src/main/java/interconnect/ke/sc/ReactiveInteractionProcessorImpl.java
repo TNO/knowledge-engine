182f940ce842a17da3028cd4b7d1558db4690658
Han Kruiger <han.kruiger@tno.nl>
2021-01-07 14:54:30 +0100

    Flesh out ReactiveInteractionProcessorImpl some more.

--- src/main/java/interconnect/ke/sc/ReactiveInteractionProcessorImpl.java
+++ src/main/java/interconnect/ke/sc/ReactiveInteractionProcessorImpl.java
@@ -16,38 +16,41 @@ import interconnect.ke.messaging.ReactMessage;
 
 public class ReactiveInteractionProcessorImpl implements ReactiveInteractionProcessor {
 	private static final Logger LOG = LoggerFactory.getLogger(ReactiveInteractionProcessorImpl.class);
-	// private MyKnowledgeBaseStore myKnowledgeBaseStore;
+	private MyKnowledgeBaseStore myKnowledgeBaseStore;
 	// private MyMetaKnowledgeBase myMetaKnowledgeBase;
 
 	@Override
 	public CompletableFuture<AnswerMessage> processAsk(AskMessage anAskMsg) {
-		// look at the graph pattern of anAskMsg.toKnowledgeInteraction
-		URI oneofOurKIs = anAskMsg.getToKnowledgeInteraction(); // This should be asn URI to one of our KIs
-		KnowledgeInteraction my;
+		URI answerKnowledgeInteractionId = anAskMsg.getToKnowledgeInteraction();
+		AnswerKnowledgeInteraction answerKnowledgeInteraction;
 		try {
-			my = null; // TODO: Ask the MyKnowledgeBaseStore to dereference the KI.
+			answerKnowledgeInteraction = (AnswerKnowledgeInteraction) myKnowledgeBaseStore.getKnowledgeInteractionById(answerKnowledgeInteractionId).getKnowledgeInteraction();
 		} catch (Throwable t) {
-			LOG.warn("Encountered an unresolvable KnowledgeInteraction ID '" + oneofOurKIs + "' that was expected to resolve to one of our own.");
+			LOG.warn("Encountered an unresolvable KnowledgeInteraction ID '" + answerKnowledgeInteractionId + "' that was expected to resolve to one of our own.");
 			var future = new CompletableFuture<AnswerMessage>();
 			future.completeExceptionally(t);
 			return future;
 		}
 		
 		var future = new CompletableFuture<AnswerMessage>();
-		BindingSet bindings = new BindingSet();
-		if (my.getIsMeta()) {
-			// TODO: Ask MyMetaKnowledgeBase for the bindings.
-		} else {
-			if (my instanceof AnswerKnowledgeInteraction) {
-				// TODO: Ask the MyKnowledgeBaseStore for the handler and call it to get the bindings (in a new thread?)
+		{
+			BindingSet bindings = null;
+			if (answerKnowledgeInteraction.getIsMeta()) {
+				// TODO: Ask MyMetaKnowledgeBase for the bindings.
 			} else {
-				LOG.warn("Encountered a KnowledgeInteraction with ID '" + oneofOurKIs + "' that unexpectedly wasn't an AnswerKnowledgeInteraction.");
-				future.completeExceptionally(new Exception("This AskMessage was unable to be ansered because it looks like it doesn't have an AnswerKnowledgeInteraction."));
-				return future;
+				var handler = myKnowledgeBaseStore.getAnswerHandler(answerKnowledgeInteractionId);
+				bindings = handler.answer(answerKnowledgeInteraction, anAskMsg.getBindings());
 			}
+	
+			AnswerMessage result = new AnswerMessage(
+				anAskMsg.getToKnowledgeBase(), answerKnowledgeInteractionId,
+				anAskMsg.getFromKnowledgeBase(), anAskMsg.getFromKnowledgeInteraction(),
+				anAskMsg.getMessageId(), bindings
+			);
+			// TODO: Here I just complete the future in the same thread, but we should
+			// figure out how to do it asynchronously.
+			future.complete(result);
 		}
-		AnswerMessage result = new AnswerMessage(anAskMsg.getToKnowledgeBase(), oneofOurKIs, anAskMsg.getFromKnowledgeBase(), anAskMsg.getFromKnowledgeInteraction(), anAskMsg.getMessageId(), bindings);
-		future.complete(result);
 		return future;
 	}
 
