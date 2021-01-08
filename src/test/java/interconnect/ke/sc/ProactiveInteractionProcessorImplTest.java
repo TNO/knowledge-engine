f0f4275167b45d466f87f1da68f7c5b5fc82e99b
Barry Nouwt <barry.nouwt@tno.nl>
2021-01-08 09:11:27 +0100

    It works.

--- src/test/java/interconnect/ke/sc/ProactiveInteractionProcessorImplTest.java
+++ src/test/java/interconnect/ke/sc/ProactiveInteractionProcessorImplTest.java
@@ -1,5 +1,6 @@
 package interconnect.ke.sc;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -8,6 +9,7 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -41,6 +43,11 @@ public class ProactiveInteractionProcessorImplTest {
 	private String subject2 = null;
 	private String object2 = null;
 
+	private URI knowledgeBaseId3 = null;
+	public String graphPattern3 = null;
+	private String subject3 = null;
+	private String object3 = null;
+
 	@BeforeEach
 	void setUp() throws Exception {
 
@@ -50,10 +57,17 @@ public class ProactiveInteractionProcessorImplTest {
 
 		this.knowledgeBaseId1 = new URI("https://www.tno.nl/interconnect/kb1");
 		this.graphPattern1 = "?s1 <https://www.tno.nl/example/predicate1> ?o1 .";
+		
 		this.knowledgeBaseId2 = new URI("https://www.tno.nl/interconnect/kb2");
 		this.graphPattern2 = "?s2 <https://www.tno.nl/example/predicate1> ?o2 .";
 		this.subject2 = "<http://www.tno.nl/subject2>";
 		this.object2 = "<https://www.tno.nl/object2>";
+
+		this.knowledgeBaseId3 = new URI("https://www.tno.nl/interconnect/kb3");
+		this.graphPattern3 = "?s3 <https://www.tno.nl/example/predicate1> ?o3 .";
+		this.subject3 = "<http://www.tno.nl/subject3>";
+		this.object3 = "<https://www.tno.nl/object3>";
+
 	}
 
 	@Test
@@ -65,12 +79,26 @@ public class ProactiveInteractionProcessorImplTest {
 
 		BindingSet bindings = future.get().getBindings();
 
-		Binding b = bindings.iterator().next();
-		assertNotNull(b);
-		assertTrue(b.containsKey("s2"));
-		assertTrue(b.get("s2").equals(subject2));
-		assertTrue(b.containsKey("o2"));
-		assertTrue(b.get("o2").equals(object2));
+		Iterator<Binding> iterator = bindings.iterator();
+
+		Binding b;
+		for (int i = 1; i < 3; i++) // we expect two bindings, but we do not know the ordering.
+		{
+			assertTrue(iterator.hasNext(), "there should be a binding number " + i);
+			b = iterator.next();
+			assertNotNull(b);
+			if (b.containsKey("s2")) {
+				assertTrue(b.get("s2").equals(subject2));
+				assertTrue(b.containsKey("o2"));
+				assertTrue(b.get("o2").equals(object2));
+			} else if (b.containsKey("s3")) {
+				assertTrue(b.get("s3").equals(subject3));
+				assertTrue(b.containsKey("o3"));
+				assertTrue(b.get("o3").equals(object3));
+			} else {
+				fail("Every binding should contain either s2 or s3.");
+			}
+		}
 	}
 
 	public class TestOtherKnowledgeBaseStore implements OtherKnowledgeBaseStore {
@@ -79,13 +107,22 @@ public class ProactiveInteractionProcessorImplTest {
 		public List<OtherKnowledgeBase> getOtherKnowledgeBases() {
 
 			List<OtherKnowledgeBase> others = new ArrayList<>();
-			List<KnowledgeInteraction> someKIs = new ArrayList<>();
-			someKIs.add(new AnswerKnowledgeInteraction(null,
-					new GraphPattern(ProactiveInteractionProcessorImplTest.this.graphPattern2)));
 
-			OtherKnowledgeBase other = new OtherKnowledgeBase(
-					ProactiveInteractionProcessorImplTest.this.knowledgeBaseId2, "", "", someKIs, null);
-			others.add(other);
+			// create/add second
+			List<KnowledgeInteraction> someKIs2 = new ArrayList<>();
+			someKIs2.add(new AnswerKnowledgeInteraction(null,
+					new GraphPattern(ProactiveInteractionProcessorImplTest.this.graphPattern2)));
+			OtherKnowledgeBase other2 = new OtherKnowledgeBase(
+					ProactiveInteractionProcessorImplTest.this.knowledgeBaseId2, "", "", someKIs2, null);
+			others.add(other2);
+
+			// create/add third
+			List<KnowledgeInteraction> someKIs3 = new ArrayList<>();
+			someKIs3.add(new AnswerKnowledgeInteraction(null,
+					new GraphPattern(ProactiveInteractionProcessorImplTest.this.graphPattern3)));
+			OtherKnowledgeBase other3 = new OtherKnowledgeBase(
+					ProactiveInteractionProcessorImplTest.this.knowledgeBaseId3, "", "", someKIs3, null);
+			others.add(other3);
 
 			return others;
 		}
