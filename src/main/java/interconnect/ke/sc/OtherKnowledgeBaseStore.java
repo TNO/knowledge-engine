e2b2f8248d04b3333f832d429ce25152788685ec
Barry Nouwt <barry.nouwt@tno.nl>
2021-01-08 14:47:28 +0100

    First version of OtherKnowledgeBaseStore

--- src/main/java/interconnect/ke/sc/OtherKnowledgeBaseStore.java
+++ src/main/java/interconnect/ke/sc/OtherKnowledgeBaseStore.java
@@ -1,35 +1,25 @@
 package interconnect.ke.sc;
 
-import java.util.List;
+import java.util.Set;
 
-import interconnect.ke.api.PostResult;
+import interconnect.ke.api.KnowledgeBase;
 import interconnect.ke.api.interaction.KnowledgeInteraction;
+import interconnect.ke.api.runtime.KnowledgeDirectory;
 
 /**
  * An {@link OtherKnowledgeBaseStore} is responsible for keeping track of
  * metadata about knowledge bases in the knowledge network. In the MVP, it
  * should poll the network periodically for other {@link KnowledgeBase}s'
- * {@link KnowledgeInteraction}s and their {@link SmartConnectorImpl}s' endpoints.
+ * {@link KnowledgeInteraction}s and their {@link SmartConnectorImpl}s'
+ * endpoints.
  * 
  * It uses the {@link KnowledgeDirectory} to discover other smart connectors.
  */
 public interface OtherKnowledgeBaseStore {
+
 	/**
 	 * @return The current list of {@link OtherKnowledgeBase}s.
 	 */
-	public List<OtherKnowledgeBase> getOtherKnowledgeBases();
+	public Set<OtherKnowledgeBase> getOtherKnowledgeBases();
 
-	/**
-	 * Synchronize the store with metadata about other knowledge bases.
-	 *
-	 * TODO: Do we really want to leave it up to the caller of this method to
-	 * provide a valid PostResult or do we want to constrain this some more?
-	 * 
-	 * TODO: (After MVP) What about knowledge bases that stopped?
-	 *
-	 * @param aPostResult A {@link PostResult} that contains bindings that have
-	 * metadata about other knowledge bases. It MUST contain bindings for an
-	 * agreed set of variables that describe other knowledge bases.
-	 */
-	// public void synchronizeOtherKnowledeBaseMetadata(PostResult aPostResult);
 }
