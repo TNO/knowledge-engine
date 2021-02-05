3c526c0c64a9abb4cd3973041ae1085339c6278a
Barry Nouwt <barry.nouwt@tno.nl>
2021-02-05 14:54:27 +0100

    Made public.

--- src/main/java/interconnect/ke/sc/Vocab.java
+++ src/main/java/interconnect/ke/sc/Vocab.java
@@ -8,20 +8,21 @@ public class Vocab {
 
 	public static final String ONTO_URI = "https://www.tno.nl/energy/ontology/interconnect#";
 
-	static final Resource KNOWLEDGE_BASE = ResourceFactory.createResource(ONTO_URI + "KnowledgeBase");
-	static final Resource KNOWLEDGE_INTERACTION = ResourceFactory.createResource(ONTO_URI + "KnowledgeInteraction");
-	static final Resource REACT_KI = ResourceFactory.createResource(ONTO_URI + "ReactKnowledgeInteraction");
-	static final Resource POST_KI = ResourceFactory.createResource(ONTO_URI + "PostKnowledgeInteraction");
-	static final Resource ANSWER_KI = ResourceFactory.createResource(ONTO_URI + "AnswerKnowledgeInteraction");
-	static final Resource ASK_KI = ResourceFactory.createResource(ONTO_URI + "AskKnowledgeInteraction");
+	public static final Resource KNOWLEDGE_BASE = ResourceFactory.createResource(ONTO_URI + "KnowledgeBase");
+	public static final Resource KNOWLEDGE_INTERACTION = ResourceFactory
+			.createResource(ONTO_URI + "KnowledgeInteraction");
+	public static final Resource REACT_KI = ResourceFactory.createResource(ONTO_URI + "ReactKnowledgeInteraction");
+	public static final Resource POST_KI = ResourceFactory.createResource(ONTO_URI + "PostKnowledgeInteraction");
+	public static final Resource ANSWER_KI = ResourceFactory.createResource(ONTO_URI + "AnswerKnowledgeInteraction");
+	public static final Resource ASK_KI = ResourceFactory.createResource(ONTO_URI + "AskKnowledgeInteraction");
 
-	static final Property HAS_GP = ResourceFactory.createProperty(ONTO_URI + "hasGraphPattern");
-	static final Property HAS_NAME = ResourceFactory.createProperty(ONTO_URI + "hasName");
-	static final Property HAS_DESCR = ResourceFactory.createProperty(ONTO_URI + "hasDescription");
-	static final Property HAS_KI = ResourceFactory.createProperty(ONTO_URI + "hasKnowledgeInteraction");
-	static final Property IS_META = ResourceFactory.createProperty(ONTO_URI + "isMeta");
-	static final Property HAS_PATTERN = ResourceFactory.createProperty(ONTO_URI + "hasPattern");
-	static final Property HAS_ARG = ResourceFactory.createProperty(ONTO_URI + "hasArgumentGraphPattern");
-	static final Property HAS_RES = ResourceFactory.createProperty(ONTO_URI + "hasResultGraphPattern");
+	public static final Property HAS_GP = ResourceFactory.createProperty(ONTO_URI + "hasGraphPattern");
+	public static final Property HAS_NAME = ResourceFactory.createProperty(ONTO_URI + "hasName");
+	public static final Property HAS_DESCR = ResourceFactory.createProperty(ONTO_URI + "hasDescription");
+	public static final Property HAS_KI = ResourceFactory.createProperty(ONTO_URI + "hasKnowledgeInteraction");
+	public static final Property IS_META = ResourceFactory.createProperty(ONTO_URI + "isMeta");
+	public static final Property HAS_PATTERN = ResourceFactory.createProperty(ONTO_URI + "hasPattern");
+	public static final Property HAS_ARG = ResourceFactory.createProperty(ONTO_URI + "hasArgumentGraphPattern");
+	public static final Property HAS_RES = ResourceFactory.createProperty(ONTO_URI + "hasResultGraphPattern");
 
 }
