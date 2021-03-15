7f19d10d5169105d40ea337e903930b84a022993
Han Kruiger <han.kruiger@tno.nl>
2021-03-15 17:35:21 +0100

    Implement SC POST

--- rest-api/src/main/java/eu/interconnectproject/knowlege_engine/rest/api/impl/ScApiServiceImpl.java
+++ rest-api/src/main/java/eu/interconnectproject/knowlege_engine/rest/api/impl/ScApiServiceImpl.java
@@ -1,5 +1,8 @@
 package eu.interconnectproject.knowlege_engine.rest.api.impl;
 
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,7 +13,9 @@ import javax.ws.rs.core.SecurityContext;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import eu.interconnectproject.knowledge_engine.smartconnector.api.KnowledgeBase;
 import eu.interconnectproject.knowledge_engine.smartconnector.api.SmartConnector;
+import eu.interconnectproject.knowledge_engine.smartconnector.impl.SmartConnectorBuilder;
 import eu.interconnectproject.knowlege_engine.rest.api.ApiResponseMessage;
 import eu.interconnectproject.knowlege_engine.rest.api.NotFoundException;
 import eu.interconnectproject.knowlege_engine.rest.api.ScApiService;
@@ -23,7 +28,68 @@ public class ScApiServiceImpl extends ScApiService {
 
 	private static final Logger LOG = LoggerFactory.getLogger(ScApiServiceImpl.class);
 
-	private Map<eu.interconnectproject.knowlege_engine.rest.model.SmartConnector, SmartConnector> connectors;
+	private Map<URI, SmartConnector> connectors = new HashMap<>();
+
+	@Override
+	public Response scPost(InlineObject req, SecurityContext securityContext) throws NotFoundException {
+		URI nonFinalKbId;
+		try {
+			nonFinalKbId = new URI(req.getKnowledgeBaseId());
+		} catch (URISyntaxException e) {
+			return Response.status(400).entity("Knowledge base ID must be a valid URI.").build();
+		}
+
+		// Not-so-nice hack to have it be a `final` variable and otherwise respond with status 400.
+		final URI kbId = nonFinalKbId;
+		final String kbDescription = req.getKnowledgeBaseDescription();
+		final String kbName = req.getKnowledgeBaseName();
+
+		if (this.connectors.containsKey(kbId)) {
+			return Response.status(400).entity("That knowledge base ID is already in use.").build();
+		}
+
+		SmartConnector sc = SmartConnectorBuilder.newSmartConnector(new KnowledgeBase(){
+			@Override
+			public URI getKnowledgeBaseId() {
+				return kbId;
+			}
+
+			@Override
+			public String getKnowledgeBaseName() {
+				return kbName;
+			}
+
+			@Override
+			public String getKnowledgeBaseDescription() {
+				return kbDescription;
+			}
+
+			@Override
+			public void smartConnectorReady(SmartConnector aSC) {
+				// Do nothing. The REST API doesn't provide these signals (yet).
+			}
+
+			@Override
+			public void smartConnectorConnectionLost(SmartConnector aSC) {
+				// Do nothing. The REST API doesn't provide these signals (yet).
+			}
+
+			@Override
+			public void smartConnectorConnectionRestored(SmartConnector aSC) {
+				// Do nothing. The REST API doesn't provide these signals (yet).
+			}
+
+			@Override
+			public void smartConnectorStopped(SmartConnector aSC) {
+				// Do nothing. The REST API doesn't provide these signals (yet).
+			}
+		}).create();
+
+		// Store it in the map.
+		this.connectors.put(kbId, sc);
+
+		return Response.ok().build();
+	}
 
 	@Override
 	public Response scAskPost(@NotNull String knowledgeBaseId, @NotNull String knowledgeInteractionId,
@@ -90,13 +156,6 @@ public class ScApiServiceImpl extends ScApiService {
 		// do some magic!
 		LOG.info("scKiPost()");
 		return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "scKiPost!")).build();
-	}
-
-	@Override
-	public Response scPost(InlineObject inlineObject, SecurityContext securityContext) throws NotFoundException {
-		// do some magic!
-		LOG.info("scPost()");
-		return Response.ok().entity(new ApiResponseMessage(ApiResponseMessage.OK, "scPost!")).build();
 	}
 
 	@Override
