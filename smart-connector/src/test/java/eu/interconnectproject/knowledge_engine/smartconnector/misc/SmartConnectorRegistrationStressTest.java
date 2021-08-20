739382f77033260cabe0d8dbf31ec224688bf2ed
Han Kruiger <han.kruiger@tno.nl>
2021-08-20 11:57:18 +0200

    Reproduce slow creation of smart connector

---
+++ smart-connector/src/test/java/eu/interconnectproject/knowledge_engine/smartconnector/misc/SmartConnectorRegistrationStressTest.java
@@ -0,0 +1,65 @@
+package eu.interconnectproject.knowledge_engine.smartconnector.misc;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eu.interconnectproject.knowledge_engine.smartconnector.api.KnowledgeNetwork;
+import eu.interconnectproject.knowledge_engine.smartconnector.api.MockedKnowledgeBase;
+import eu.interconnectproject.knowledge_engine.smartconnector.api.SmartConnector;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class SmartConnectorRegistrationStressTest {
+	private static final Logger LOG = LoggerFactory.getLogger(SmartConnectorRegistrationStressTest.class);
+
+	private KnowledgeNetwork kn;
+	private static final int NUM_KBS = 30;
+
+	private static final int TEST_FAIL_THRESHOLD_SECONDS = 10;
+
+	@BeforeAll
+	void seedKnowledgeNetwork() {
+		kn = new KnowledgeNetwork();
+		for (var i = 0; i < NUM_KBS; i++) {
+			LOG.info("Starting KB{}", i);
+			kn.addKB(new MockedKnowledgeBase("INITIAL-KB" + i));
+		}
+		kn.startAndWaitForReady();
+		kn.waitForUpToDate();
+	}
+
+	@Test
+	public void testRegisterWhenManySmartConnectorsExist() throws ExecutionException, InterruptedException {
+		Instant beforeRegistration = Instant.now();
+
+		var future = new CompletableFuture<Void>();
+
+		var kb = new MockedKnowledgeBase("TEST-KB") {
+			@Override
+			public void smartConnectorReady(SmartConnector sc) {
+				future.complete(null);
+			}
+		};
+		kn.addKB(kb); // Might not even be necessary.
+		
+		kb.start(); // Triggers the creation of the smart connector
+
+		future.get(); // Waits for the future.
+
+		Instant afterRegistration = Instant.now();
+
+		Duration duration = Duration.between(beforeRegistration, afterRegistration);
+
+		LOG.info("Registration took {} milliseconds.", duration.toMillis());
+		assertTrue(duration.toSeconds() < TEST_FAIL_THRESHOLD_SECONDS);
+	}
+}
