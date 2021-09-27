113fb7fd65eb306b5fd9f992fc4d9b5a39d0c2fd
Barry Nouwt <barry.nouwt@tno.nl>
2021-09-27 12:05:37 +0200

    Also removed other InterConnect references.
    Note:
    - removed references from the REST API specs

--- smart-connector/src/main/java/eu/knowledge/engine/smartconnector/impl/package-info.java
+++ smart-connector/src/main/java/eu/knowledge/engine/smartconnector/impl/package-info.java
@@ -1,5 +1,5 @@
 /**
  * This package contains all the classes related to the implementation of the
- * {@link interconnect.ke.sc.SmartConnectorImpl} interface.
+ * {@link eu.knowledge.engine.smartconnector.impl.SmartConnectorImpl} interface.
  */
 package eu.knowledge.engine.smartconnector.impl;
