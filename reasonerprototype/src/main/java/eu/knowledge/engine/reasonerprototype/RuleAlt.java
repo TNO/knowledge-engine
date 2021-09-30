7916531347b2a1d280762eb45b5161ebf9e70694
Barry Nouwt <barry.nouwt@tno.nl>
2021-09-30 13:59:53 +0200

    New algorithm for graph pattern matching.

--- reasonerprototype/src/main/java/eu/knowledge/engine/reasonerprototype/RuleAlt.java
+++ reasonerprototype/src/main/java/eu/knowledge/engine/reasonerprototype/RuleAlt.java
@@ -68,21 +68,62 @@ public class RuleAlt {
 		return matches;
 	}
 
-	public Set<Map<TriplePattern, TriplePattern>> consequentMatches2(Set<TriplePattern> objective) {
-		Set<Map<TriplePattern, TriplePattern>> matches = new HashSet<>();
+	public BindingSetHandler getBindingSetHandler() {
+		return bindingSetHandler;
+	}
 
+	private Set<Map<TriplePattern, TriplePattern>> matches2(Set<TriplePattern> objective, Set<TriplePattern> consequent,
+			boolean fullMatchOnly) {
+
+		assert objective != null;
+		assert consequent != null;
+		assert !objective.isEmpty();
+		assert !consequent.isEmpty();
+
+		Set<Map<TriplePattern, TriplePattern>> isos = new HashSet<>();
+
+		Map<Map<TriplePattern, TriplePattern>, Map<Value, Value>> allMatches = new HashMap<>();
+
+		Map<TriplePattern, Map<Value, Value>> consMatches;
 		for (TriplePattern objTriple : objective) {
-			for (TriplePattern consTriple : this.consequent) {
 
-			}
+			// find all possible matches of the current objective triple in the consequent
+			consMatches = containsMatchingTriplePattern(consequent, objTriple);
+
+			// store all found matches individually
+			storeConsequentMatches(objTriple, consMatches, allMatches);
+
+			// update existing matches with newly found ones (taking fullMatchOnly into
+			// account)
 
 		}
 
-		return matches;
+		assert isos != null;
+
+		return isos;
 	}
 
-	public BindingSetHandler getBindingSetHandler() {
-		return bindingSetHandler;
+	private void storeConsequentMatches(TriplePattern objTriple, Map<TriplePattern, Map<Value, Value>> consMatches,
+			Map<Map<TriplePattern, TriplePattern>, Map<Value, Value>> allMatches) {
+
+		assert consMatches != null;
+		assert allMatches != null;
+
+		Map<Map<TriplePattern, TriplePattern>, Map<Value, Value>> newAllMatches = new HashMap<>(allMatches);
+
+		for (Map.Entry<TriplePattern, Map<Value, Value>> entry : consMatches.entrySet()) {
+
+			for (Map.Entry<Map<TriplePattern, TriplePattern>, Map<Value, Value>> allMatchesEntry : newAllMatches
+					.entrySet()) {
+
+				//
+
+			}
+
+			allMatches.put(entry.getValue(), entry.getValue());
+
+		}
+
 	}
 
 	/**
@@ -103,8 +144,9 @@ public class RuleAlt {
 	private Set<Map<TriplePattern, TriplePattern>> matches(Set<TriplePattern> objective, Set<TriplePattern> consequent,
 			Map<Value, Value> context) {
 		Set<Map<TriplePattern, TriplePattern>> isos = new HashSet<>();
-
+		System.out.println("matches: " + objective + ", " + context);
 		for (TriplePattern objTriple : objective) {
+			System.out.println("next objective triple");
 			Map<TriplePattern, Map<Value, Value>> allMatches = rhsMatchesAlt(objTriple, consequent);
 
 			Set<TriplePattern> reducedObj = new HashSet<>(objective);
@@ -178,10 +220,36 @@ public class RuleAlt {
 		Map<TriplePattern, Map<Value, Value>> allMatches = new HashMap<>();
 		for (TriplePattern myTriple : rhs) {
 			Map<Value, Value> map = myTriple.matchesWithSubstitutionMap(objTriple);
-			if (map != null)
+
+			assert map != null;
+
+			if (!map.isEmpty())
 				allMatches.put(myTriple, map);
 		}
 		return allMatches;
+	}
+
+	private Map<TriplePattern, Map<Value, Value>> containsMatchingTriplePattern(Set<TriplePattern> consequent,
+			TriplePattern pattern) {
+
+		assert consequent != null;
+		assert pattern != null;
+		assert !consequent.isEmpty();
+
+		Map<TriplePattern, Map<Value, Value>> matchingTriplePatterns = new HashMap<>();
+		Map<Value, Value> map;
+		for (TriplePattern tp : consequent) {
+			map = tp.matchesWithSubstitutionMap(pattern);
+
+			assert map != null;
+
+			if (!map.isEmpty()) {
+				matchingTriplePatterns.put(tp, map);
+			}
+		}
+
+		assert matchingTriplePatterns != null;
+		return matchingTriplePatterns;
 	}
 
 	@Override
@@ -200,6 +268,100 @@ public class RuleAlt {
 			vars.addAll(this.getVars(this.consequent));
 
 		return vars;
+	}
+
+	public static class Match {
+
+		/**
+		 * keys: things that are matching values: things upon which they match
+		 */
+		private Map<TriplePattern, TriplePattern> matchingPatterns;
+
+		/**
+		 * keys: thing that is mapped values: thing upon which it maps
+		 */
+		private Map<Value, Value> mapping;
+
+		public Match(TriplePattern matchTriple, TriplePattern uponTriple, Map<Value, Value> someMapping) {
+
+			matchingPatterns = new HashMap<>();
+			mapping = new HashMap<>();
+
+			matchingPatterns.put(matchTriple, uponTriple);
+			mapping.putAll(someMapping);
+		}
+
+		/**
+		 * Only merges if they are not conflicting.
+		 * 
+		 * @param otherMatch
+		 * @return a new merged match, otherwise {@code null}.
+		 */
+		public Match merge(Match otherMatch) {
+
+			// check if the mappings do not conflict.
+
+			Map<TriplePattern, TriplePattern> newMatchingPatterns = new HashMap<>(this.matchingPatterns);
+			Map<Value, Value> newMapping = new HashMap<>(this.mapping);
+			newMatchingPatterns.putAll(otherMatch.getMatchingPatterns());
+			newMapping.putAll(otherMatch.getMappings());
+			Match m = new Match(newMatchingPatterns, newMapping);
+			return m;
+		}
+
+		private Map<Value, Value> getMappings() {
+			return this.mapping;
+		}
+
+		private Map<TriplePattern, TriplePattern> getMatchingPatterns() {
+			return this.matchingPatterns;
+		}
+
+		private Match(Map<TriplePattern, TriplePattern> someMatchingPatterns, Map<Value, Value> someMapping) {
+			this.matchingPatterns = someMatchingPatterns;
+			this.mapping = someMapping;
+		}
+
+		public Match clone() {
+			Map<TriplePattern, TriplePattern> newMatchingPatterns = new HashMap<>(this.matchingPatterns);
+			Map<Value, Value> newMapping = new HashMap<>(this.mapping);
+			return new Match(newMatchingPatterns, newMapping);
+		}
+
+		@Override
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			result = prime * result + ((mapping == null) ? 0 : mapping.hashCode());
+			result = prime * result + ((matchingPatterns == null) ? 0 : matchingPatterns.hashCode());
+			return result;
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj) {
+				return true;
+			}
+			if (!(obj instanceof Match)) {
+				return false;
+			}
+			Match other = (Match) obj;
+			if (mapping == null) {
+				if (other.mapping != null) {
+					return false;
+				}
+			} else if (!mapping.equals(other.mapping)) {
+				return false;
+			}
+			if (matchingPatterns == null) {
+				if (other.matchingPatterns != null) {
+					return false;
+				}
+			} else if (!matchingPatterns.equals(other.matchingPatterns)) {
+				return false;
+			}
+			return true;
+		}
 	}
 
 }
