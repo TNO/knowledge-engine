dfdf0d1efccba63d3217344210c938a91485370a
Han Kruiger <han.kruiger@tno.nl>
2021-11-05 17:18:02 +0100

    Manual extra validation for URIs with regex

--- smart-connector/src/test/java/eu/knowledge/engine/smartconnector/api/TestBindingValidation.java
+++ smart-connector/src/test/java/eu/knowledge/engine/smartconnector/api/TestBindingValidation.java
@@ -4,8 +4,22 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
-public class TestBindingValidationMultipleIRIs {
+public class TestBindingValidation {
 	
+	@Test
+	void testBindingNormalIri() {
+		var b = new BindingValidator();
+		b.validateValidBindingValue("<http://example.org/1>");
+	}
+
+	@Test
+	void testBindingPrefixedIri() {
+		assertThrows(IllegalArgumentException.class, () -> {
+			var b = new BindingValidator();
+			b.validateValidBindingValue("rdf:type");
+		});
+	}
+
 	@Test
 	void testBindingTwoIrisSeparatedWithSemicolon() {
 		assertThrows(IllegalArgumentException.class, () -> {
