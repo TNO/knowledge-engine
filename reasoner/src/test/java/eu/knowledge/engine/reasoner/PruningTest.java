9c3683bf16b4933629d87796749d4367c3187338
Barry Nouwt <barry.nouwt@tno.nl>
2022-03-14 19:06:08 +0100

    Added more complex test.
    Also:
    - reduce test time

--- reasoner/src/test/java/eu/knowledge/engine/reasoner/PruningTest.java
+++ reasoner/src/test/java/eu/knowledge/engine/reasoner/PruningTest.java
@@ -18,6 +18,7 @@ import eu.knowledge.engine.reasoner.api.TriplePattern;
 public class PruningTest {
 
 	private KeReasoner reasoner;
+	private Rule isInRoomRule;
 
 	@Before
 	public void init() {
@@ -38,10 +39,24 @@ public class PruningTest {
 				})));
 		reasoner.addRule(rule);
 
+		isInRoomRule = new Rule(new HashSet<>(),
+				new HashSet<>(
+						Arrays.asList(new TriplePattern("?x <type> <Sensor>"), new TriplePattern("?x <isInRoom> ?y"))),
+				new DataBindingSetHandler(new Table(new String[] {
+				//@formatter:off
+						"x", "y"
+						//@formatter:on
+				}, new String[] {
+				//@formatter:off
+						"<sensor1>,<room1>",
+						"<sensor2>,<room2>",
+						//@formatter:on
+				})));
+
 	}
 
 	@Test
-	public void test() {
+	public void testSingleChild() {
 		Binding b = new Binding();
 		Set<TriplePattern> objective = new HashSet<>();
 		objective.add(new TriplePattern("?p <type> <Sensor>"));
@@ -68,5 +83,37 @@ public class PruningTest {
 		System.out.println("bindings: " + bind);
 		assertTrue(bind.isEmpty());
 	}
+	
+	@Test
+	public void testMultipleChildren() {
+		Binding b = new Binding();
+		Set<TriplePattern> objective = new HashSet<>();
+		objective.add(new TriplePattern("?p <type> <Sensor>"));
+		objective.add(new TriplePattern("?p <hasValInC> ?q"));
+		objective.add(new TriplePattern("?p <isInRoom> ?r"));
+		objective.add(new TriplePattern("?p <hasOwner> ?r"));	//knowledge gap
 
+		reasoner.addRule(this.isInRoomRule);
+		
+		TaskBoard taskboard = new TaskBoard();
+
+		// Start reasoning
+		ReasoningNode root = reasoner.backwardPlan(objective, MatchStrategy.FIND_ONLY_BIGGEST_MATCHES, taskboard);
+		System.out.println(root);
+
+		Map<TriplePattern, Set<ReasoningNode>> findAntecedentCoverage = root.findAntecedentCoverage();
+		BindingSet bs = new BindingSet();
+		Binding binding2 = new Binding();
+		bs.add(binding2);
+
+		BindingSet bind;
+		while ((bind = root.continueBackward(bs)) == null) {
+			System.out.println(root);
+			taskboard.executeScheduledTasks();
+		}
+
+		System.out.println("bindings: " + bind);
+		assertTrue(bind.isEmpty());
+	}
+	
 }
