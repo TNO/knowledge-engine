a850dd3465f38ac78dabba2d49a444a02d97b27e
Barry Nouwt <barry.nouwt@tno.nl>
2022-06-21 11:37:11 +0200

    Refactored the semantic composition test.
    It should be a bit more readable.

--- smart-connector/src/test/java/eu/knowledge/engine/smartconnector/api/TestDynamicSemanticComposition.java
+++ smart-connector/src/test/java/eu/knowledge/engine/smartconnector/api/TestDynamicSemanticComposition.java
@@ -38,18 +38,125 @@ public class TestDynamicSemanticComposition {
 
 	private static KnowledgeNetwork kn;
 
+	private static PrefixMappingMem prefixes;
+
+	private AskKnowledgeInteraction askKI;
+	private PostKnowledgeInteraction postKI;
+
+	private static Set<Rule> ruleSet;
+
 	@BeforeAll
 	public static void setup() throws InterruptedException, BrokenBarrierException, TimeoutException {
+
+		prefixes = new PrefixMappingMem();
+		prefixes.setNsPrefixes(PrefixMapping.Standard);
+		prefixes.setNsPrefix("ex", "https://www.tno.nl/example/");
+//		prefixes.setNsPrefix("tno", "https://www.tno.nl/");
+		prefixes.setNsPrefix("v1905", "https://www.tno.nl/defense/ontology/v1905/");
+
+		// add extra domain knowledge in the form of a rule to kbHVTSearcher.
+		HashSet<TriplePattern> consequent1 = new HashSet<TriplePattern>();
+		consequent1.add(new TriplePattern(
+				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/HighValueTarget>"));
+		HashSet<TriplePattern> antecedent1 = new HashSet<TriplePattern>();
+		antecedent1.add(new TriplePattern(
+				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/Target>"));
+		antecedent1.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasCountry> \"Russia\""));
+
+		HashSet<TriplePattern> consequent2 = new HashSet<TriplePattern>();
+		consequent2.add(new TriplePattern(
+				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/HighValueTarget>"));
+		HashSet<TriplePattern> antecedent2 = new HashSet<TriplePattern>();
+		antecedent2.add(new TriplePattern(
+				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/Target>"));
+		antecedent2.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasLanguage> \"Russian\""));
+
+		HashSet<TriplePattern> consequent3 = new HashSet<TriplePattern>();
+		consequent3.add(new TriplePattern(
+				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/HighValueTarget>"));
+		HashSet<TriplePattern> antecedent3 = new HashSet<TriplePattern>();
+		antecedent3.add(new TriplePattern(
+				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/Target>"));
+		antecedent3.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasLanguage> \"Russian\""));
+		antecedent3.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasCountry> \"Russia\""));
+
+		ruleSet = new HashSet<>();
+		ruleSet.add(new Rule(antecedent1, consequent1));
+		ruleSet.add(new Rule(antecedent2, consequent2));
+//		ruleSet.add(new Rule(antecedent3, consequent3));
+
 	}
 
 	@Test
 	public void testAskAnswer() throws InterruptedException, URISyntaxException {
 
-		PrefixMappingMem prefixes = new PrefixMappingMem();
-		prefixes.setNsPrefixes(PrefixMapping.Standard);
-		prefixes.setNsPrefix("ex", "https://www.tno.nl/example/");
-//		prefixes.setNsPrefix("tno", "https://www.tno.nl/");
-		prefixes.setNsPrefix("v1905", "https://www.tno.nl/defense/ontology/v1905/");
+		setupNetwork();
+		kn.sync();
+
+		// start planning ask for targets!
+		BindingSet bindings = null;
+		AskPlan plan = kbHVTSearcher.planAsk(askKI, new RecipientSelector());
+		ReasoningNode rn = plan.getReasoningNode();
+		LOG.info("Plan: {}", rn);
+		// check for knowledge gaps
+		Set<Set<TriplePattern>> gaps = Util.getKnowledgeGaps(rn);
+		LOG.info("Found gaps: " + gaps);
+
+		// add KB that fills the knowledge gap
+		updateNetwork();
+		kn.sync();
+
+		// start testing ask for targets!
+		bindings = null;
+		try {
+			AskResult result = kbHVTSearcher.ask(askKI, new BindingSet()).get();
+			bindings = result.getBindings();
+			// try to generate JSON tree.
+			TestUtils.printSequenceDiagram(kbHVTSearcher.getKnowledgeBaseId().toString(), "ask", postKI.getArgument(),
+					result.getReasoningNode(), prefixes);
+		} catch (InterruptedException | ExecutionException e) {
+			fail();
+		}
+
+		Iterator<Binding> iter = bindings.iterator();
+		LOG.info("Result bindings are: {}", bindings);
+
+		assertTrue(iter.hasNext(), "there should be at least 1 binding");
+		Binding b = iter.next();
+
+		assertEquals("<https://www.tno.nl/example/target1>", b.get("id"), "Binding of 'id' is incorrect.");
+		assertEquals("\"Bla\"", b.get("name"), "Binding of 'name' is incorrect.");
+
+		assertFalse(iter.hasNext(), "This BindingSet should only have a single binding");
+
+		// start testing post of targets!
+		BindingSet bindingSet = new BindingSet();
+		Binding binding = new Binding();
+		binding.put("id", "<https://www.tno.nl/example/target1>");
+		binding.put("name", "\"Bla\"");
+		bindingSet.add(binding);
+
+		try {
+			LOG.info("Before post!");
+			PostPlan aPlan = kbTargetObserver.planPost(postKI, new RecipientSelector());
+			PostResult result = aPlan.execute(bindingSet).get();
+			bindings = result.getBindings();
+			iter = bindings.iterator();
+			assertFalse(iter.hasNext(), "there should be no bindings");
+			LOG.info("After post!");
+
+//			TestUtils.printReasoningNodeDotNotation("TargetObserver", aPlan.getReasoningNode());
+
+			// try to generate JSON tree.
+			TestUtils.printSequenceDiagram(kbTargetObserver.getKnowledgeBaseId().toString(), "post",
+					postKI.getArgument(), result.getReasoningNode(), prefixes);
+		} catch (Exception e) {
+			LOG.error("Error", e);
+		}
+
+	}
+
+	private void setupNetwork() {
 
 		kn = new KnowledgeNetwork();
 		// start a knowledge base with the behaviour "I can supply observations of
@@ -87,15 +194,14 @@ public class TestDynamicSemanticComposition {
 			return bindingSet;
 		});
 		// and a post pattern to publish newly observed Targets
-		PostKnowledgeInteraction postKI = new PostKnowledgeInteraction(new CommunicativeAct(), gp1, null,
-				"postTargets");
-		kbTargetObserver.register(postKI);
+		this.postKI = new PostKnowledgeInteraction(new CommunicativeAct(), gp1, null, "postTargets");
+		kbTargetObserver.register(this.postKI);
 
 		// Patterns for the HVTSearcher
 		// a pattern to ask for High Value Target searches
 		GraphPattern gp2 = new GraphPattern(prefixes, "?id rdf:type v1905:HighValueTarget . ?id v1905:hasName ?name .");
-		AskKnowledgeInteraction askKI = new AskKnowledgeInteraction(new CommunicativeAct(), gp2, "askHVTargets");
-		kbHVTSearcher.register(askKI);
+		this.askKI = new AskKnowledgeInteraction(new CommunicativeAct(), gp2, "askHVTargets");
+		kbHVTSearcher.register(this.askKI);
 		// a pattern to react to incoming new High Value Targets
 		ReactKnowledgeInteraction reactKIsearcher = new ReactKnowledgeInteraction(new CommunicativeAct(), gp2, null);
 		kbHVTSearcher.register(reactKIsearcher, (anRKI, aReactExchangeInfo) -> {
@@ -110,55 +216,12 @@ public class TestDynamicSemanticComposition {
 			return new BindingSet();
 		});
 
-		// add extra domain knowledge in the form of a rule to kbHVTSearcher.
-		HashSet<TriplePattern> consequent1 = new HashSet<TriplePattern>();
-		consequent1.add(new TriplePattern(
-				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/HighValueTarget>"));
-		HashSet<TriplePattern> antecedent1 = new HashSet<TriplePattern>();
-		antecedent1.add(new TriplePattern(
-				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/Target>"));
-		antecedent1.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasCountry> \"Russia\""));
-
-		HashSet<TriplePattern> consequent2 = new HashSet<TriplePattern>();
-		consequent2.add(new TriplePattern(
-				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/HighValueTarget>"));
-		HashSet<TriplePattern> antecedent2 = new HashSet<TriplePattern>();
-		antecedent2.add(new TriplePattern(
-				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/Target>"));
-		antecedent2.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasLanguage> \"Russian\""));
-
-		HashSet<TriplePattern> consequent3 = new HashSet<TriplePattern>();
-		consequent3.add(new TriplePattern(
-				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/HighValueTarget>"));
-		HashSet<TriplePattern> antecedent3 = new HashSet<TriplePattern>();
-		antecedent3.add(new TriplePattern(
-				"?id <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <https://www.tno.nl/defense/ontology/v1905/Target>"));
-		antecedent3.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasLanguage> \"Russian\""));
-		antecedent3.add(new TriplePattern("?id <https://www.tno.nl/defense/ontology/v1905/hasCountry> \"Russia\""));
-
-		Set<Rule> ruleSet = new HashSet<>();
-		ruleSet.add(new Rule(antecedent1, consequent1));
-		ruleSet.add(new Rule(antecedent2, consequent2));
-//		ruleSet.add(new Rule(antecedent3, consequent3));
-
 		kbHVTSearcher.setDomainKnowledge(ruleSet);
 		kbTargetObserver.setDomainKnowledge(ruleSet);
 
-		kn.sync();
+	}
 
-		// start planning ask for targets!
-		BindingSet bindings = null;
-		AskPlan plan = kbHVTSearcher.planAsk(askKI, new RecipientSelector());
-
-		ReasoningNode rn = plan.getReasoningNode();
-//		rn.prune();
-		LOG.info("Plan: {}", rn);
-
-		// check for knowledge gaps
-		Set<Set<TriplePattern>> gaps = Util.getKnowledgeGaps(rn);
-
-		LOG.info("Found gaps: " + gaps);
-		// add KB that fills the knowledge gap
+	private void updateNetwork() {
 
 		// start a knowledge base with the behaviour "Give me a target and I can supply
 		// its basic attributes"
@@ -247,59 +310,6 @@ public class TestDynamicSemanticComposition {
 			return resultBindings;
 		});
 		kbTargetLanguageSupplier.setDomainKnowledge(ruleSet);
-		kn.sync();
-
-		// start testing ask for targets!
-		bindings = null;
-		try {
-			LOG.trace("Before ask.");
-			AskResult result = kbHVTSearcher.ask(askKI, new BindingSet()).get();
-			bindings = result.getBindings();
-			LOG.trace("After ask.");
-			// try to generate JSON tree.
-			TestUtils.printSequenceDiagram(kbHVTSearcher.getKnowledgeBaseId().toString(), "ask", postKI.getArgument(),
-					result.getReasoningNode(), prefixes);
-		} catch (InterruptedException | ExecutionException e) {
-			fail();
-		}
-
-		Iterator<Binding> iter = bindings.iterator();
-		LOG.info("Result bindings are: {}", bindings);
-
-		assertTrue(iter.hasNext(), "there should be at least 1 binding");
-		Binding b = iter.next();
-
-		assertEquals("<https://www.tno.nl/example/target1>", b.get("id"), "Binding of 'id' is incorrect.");
-//		assertEquals("\"Bla\"^^<http://www.w3.org/2001/XMLSchema#string>", b.get("name"), "Binding of 'name' is incorrect.");
-		assertEquals("\"Bla\"", b.get("name"), "Binding of 'name' is incorrect.");
-
-		assertFalse(iter.hasNext(), "This BindingSet should only have a single binding");
-
-		// start testing post of targets!
-		BindingSet bindingSet = new BindingSet();
-		Binding binding = new Binding();
-		binding.put("id", "<https://www.tno.nl/example/target1>");
-//		binding.put("name", "\"Bla\"^^<http://www.w3.org/2001/XMLSchema#string>");
-		binding.put("name", "\"Bla\"");
-		bindingSet.add(binding);
-
-		try {
-			LOG.info("Before post!");
-			PostPlan aPlan = kbTargetObserver.planPost(postKI, new RecipientSelector());
-			PostResult result = aPlan.execute(bindingSet).get();
-			bindings = result.getBindings();
-			iter = bindings.iterator();
-			assertFalse(iter.hasNext(), "there should be no bindings");
-			LOG.info("After post!");
-
-//			TestUtils.printReasoningNodeDotNotation("TargetObserver", aPlan.getReasoningNode());
-
-			// try to generate JSON tree.
-			TestUtils.printSequenceDiagram(kbTargetObserver.getKnowledgeBaseId().toString(), "post",
-					postKI.getArgument(), result.getReasoningNode(), prefixes);
-		} catch (Exception e) {
-			LOG.error("Error", e);
-		}
 
 	}
 
