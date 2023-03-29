4d4fe24230fbf16c7f01dbddd7ff3bfdf426a760
Barry Nouwt <barry.nouwt@tno.nl>
2023-03-29 11:50:49 +0200

    It works now, but takes more than 8 minutes.

--- smart-connector/src/test/java/eu/knowledge/engine/smartconnector/api/AdditionForAudienceTest.java
+++ smart-connector/src/test/java/eu/knowledge/engine/smartconnector/api/AdditionForAudienceTest.java
@@ -2,57 +2,38 @@ package eu.knowledge.engine.smartconnector.api;
 
 import java.util.concurrent.ExecutionException;
 
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AdditionForAudienceTest {
 
-	MockedKnowledgeBase kb1;
-	MockedKnowledgeBase kbSum;
-	MockedKnowledgeBase kbNum;
-	MockedKnowledgeBase kb10;
-	MockedKnowledgeBase kb100;
-	private AskKnowledgeInteraction askKI;
+	private static final Logger LOG = LoggerFactory.getLogger(AdditionForAudienceTest.class);
 
-	public void beforeAll() {
+	class MyReactHandler implements ReactHandler {
 
-		KnowledgeNetwork kn = new KnowledgeNetwork();
+		private int place;
 
-		kb1 = new MockedKnowledgeBase("kb1");
-		kb1.setReasonerEnabled(true);
-		kn.addKB(kb1);
+		public MyReactHandler(int aPlace) {
+			this.place = aPlace;
+		}
 
-		var reactKI = new ReactKnowledgeInteraction(new CommunicativeAct(), new GraphPattern(
-				"?e <type> <Expression> . ?e <hasNr> ?n1 . ?e <hasNr> ?n2 . ?n1 <type> <Number> . ?n1 <hasDigit> ?d1 . ?d1 <hasPlace> ?p . ?d1 <hasActualDigit> ?ad1 . ?n2 <type> <Number> . ?n2 <hasDigit> ?d2 . ?d2 <hasPlace> ?p . ?d2 <hasActualDigit> ?ad2 ."),
-				new GraphPattern(
-						"?e <hasOutcome> ?n3 . ?n3 <type> <Number> . ?n3 <hasDigit> ?d3 . ?d3 <hasPlace> ?p . ?d3 <hasActualDigit> ?ad3 . ?d3 <hasOverflow> ?o ."));
-
-		kb1.register(reactKI, (anRKI, aReactExchangeInfo) -> {
+		public BindingSet react(ReactKnowledgeInteraction anRKI, ReactExchangeInfo aReactExchangeInfo) {
 			BindingSet bs = aReactExchangeInfo.getArgumentBindings();
 
 			System.out.println("In: " + bs);
-
-			int i = 0;
 
 			BindingSet bs2 = new BindingSet();
 			for (Binding b : bs) {
 				var expr = b.get("e");
 				var n1 = b.get("n1");
 				var d1 = b.get("d1");
-				var p = b.get("p");
+				var p = "\"" + String.valueOf(place) + "\"";
 				var ad1 = b.get("ad1");
 
 				var n2 = b.get("n2");
 				var d2 = b.get("d2");
 				var ad2 = b.get("ad2");
-
-//				System.out.println(n1);
-//				System.out.println(d1);
-//				System.out.println(p);
-//				System.out.println(ad1);
-//				System.out.println(n2);
-//				System.out.println(d2);
-//				System.out.println(ad2);
 
 				if (!n1.equals(n2)) {
 
@@ -61,8 +42,8 @@ public class AdditionForAudienceTest {
 
 					int actualD3 = actualD1 + actualD2;
 
-					String n3 = "<" + i + ">";
-					String d3 = "<" + i + "d" + p.substring(1, 2) + ">";
+					String n3 = "<" + removeFirstLastChar(n1) + "/" + removeFirstLastChar(n2) + ">";
+					String d3 = "<" + removeFirstLastChar(n3) + "/digit/" + removeFirstLastChar(p) + ">";
 					String ad3 = "\"" + ((actualD3 > 9) ? ("" + actualD3).substring(1) : actualD3) + "\"";
 					String o = "\"" + (actualD3 > 9) + "\"";
 
@@ -71,39 +52,75 @@ public class AdditionForAudienceTest {
 					e.put("n3", n3);
 					e.put("d3", d3);
 					e.put("ad3", ad3);
-					e.put("p", p);
+//					e.put("p", p);
 					e.put("o", o);
 					bs2.add(e);
-					i++;
 				}
 			}
 
 			System.out.println("Out: " + bs2);
+
 			return bs2;
-		});
+		}
+	};
+
+	MockedKnowledgeBase kb1;
+	MockedKnowledgeBase kbSum;
+	MockedKnowledgeBase kbNum;
+	MockedKnowledgeBase kb10;
+	MockedKnowledgeBase kb100;
+	MockedKnowledgeBase kbRule;
+	private AskKnowledgeInteraction askKI;
+
+	public void beforeAll() {
+
+		KnowledgeNetwork kn = new KnowledgeNetwork();
+
+		// kb1
+		kb1 = new MockedKnowledgeBase("kb1");
+		kb1.setReasonerEnabled(true);
+		kn.addKB(kb1);
+
+		var reactKI = new ReactKnowledgeInteraction(new CommunicativeAct(), new GraphPattern(
+				"?e <file:///type> <file:///Expression> . ?e <file:///hasFirstNr> ?n1 . ?e <file:///hasSecondNr> ?n2 . ?n1 <file:///type> <file:///Number> . ?n1 <file:///hasDigit> ?d1 . ?d1 <file:///hasPlace> \"1\" . ?d1 <file:///hasActualDigit> ?ad1 . ?n2 <file:///type> <file:///Number> . ?n2 <file:///hasDigit> ?d2 . ?d2 <file:///hasPlace> \"1\" . ?d2 <file:///hasActualDigit> ?ad2 ."),
+				new GraphPattern(
+						"?e <file:///hasOutcome> ?n3 . ?n3 <file:///type> <file:///Number> . ?n3 <file:///hasDigit> ?d3 . ?d3 <file:///hasPlace> \"1\" . ?d3 <file:///hasActualDigit> ?ad3 . ?d3 <file:///hasOverflow> ?o ."));
+		kb1.register(reactKI, new MyReactHandler(1));
+
+		// kb10
+		kb10 = new MockedKnowledgeBase("kb10");
+		kb10.setReasonerEnabled(true);
+		kn.addKB(kb10);
+		reactKI = new ReactKnowledgeInteraction(new CommunicativeAct(), new GraphPattern(
+				"?e <file:///type> <file:///Expression> . ?e <file:///hasFirstNr> ?n1 . ?e <file:///hasSecondNr> ?n2 . ?n1 <file:///type> <file:///Number> . ?n1 <file:///hasDigit> ?d1 . ?d1 <file:///hasPlace> \"2\" . ?d1 <file:///hasActualDigit> ?ad1 . ?n2 <file:///type> <file:///Number> . ?n2 <file:///hasDigit> ?d2 . ?d2 <file:///hasPlace> \"2\" . ?d2 <file:///hasActualDigit> ?ad2 ."),
+				new GraphPattern(
+						"?e <file:///hasOutcome> ?n3 . ?n3 <file:///type> <file:///Number> . ?n3 <file:///hasDigit> ?d3 . ?d3 <file:///hasPlace> \"2\" . ?d3 <file:///hasActualDigit> ?ad3 . ?d3 <file:///hasOverflow> ?o ."));
+		kb10.register(reactKI, new MyReactHandler(2));
+
+		kb100 = new MockedKnowledgeBase("kb100");
+		kb100.setReasonerEnabled(true);
+		kn.addKB(kb100);
+		reactKI = new ReactKnowledgeInteraction(new CommunicativeAct(), new GraphPattern(
+				"?e <file:///type> <file:///Expression> . ?e <file:///hasFirstNr> ?n1 . ?e <file:///hasSecondNr> ?n2 . ?n1 <file:///type> <file:///Number> . ?n1 <file:///hasDigit> ?d1 . ?d1 <file:///hasPlace> \"3\" . ?d1 <file:///hasActualDigit> ?ad1 . ?n2 <file:///type> <file:///Number> . ?n2 <file:///hasDigit> ?d2 . ?d2 <file:///hasPlace> \"3\" . ?d2 <file:///hasActualDigit> ?ad2 ."),
+				new GraphPattern(
+						"?e <file:///hasOutcome> ?n3 . ?n3 <file:///type> <file:///Number> . ?n3 <file:///hasDigit> ?d3 . ?d3 <file:///hasPlace> \"3\" . ?d3 <file:///hasActualDigit> ?ad3 . ?d3 <file:///hasOverflow> ?o ."));
+		kb100.register(reactKI, new MyReactHandler(3));
 
 		kbSum = new MockedKnowledgeBase("kbSum");
 		kbSum.setReasonerEnabled(true);
 		kn.addKB(kbSum);
 
 		var answerKI = new AnswerKnowledgeInteraction(new CommunicativeAct(), new GraphPattern(
-				"?e <type> <Expression> . ?e <hasNr> ?n1 . ?n1 <type> <Number> . ?n1 <hasDigit> ?d1 . ?d1 <hasPlace> ?p . ?d1 <hasActualDigit> ?ad1 .")); //
+				"?e <file:///type> <file:///Expression> . ?e ?hasFirstOrSecondNumber ?n1 . ?n1 <file:///type> <file:///Number> . ?n1 <file:///hasDigit> ?d1 . ?d1 <file:///hasPlace> ?p . ?d1 <file:///hasActualDigit> ?ad1 .")); //
 
 		kbSum.register(answerKI, (anAKI, anAnswerExchangeInfo) -> {
 
 			BindingSet bs = new BindingSet();
 
-			/**
-			 * <123> <type> <Number> . <123> <hasDigit> <123d1> . <123d1> <type> <Digit> .
-			 * <123d1> <hasPlace> "1" . <123d1> <hasActualDigit> "1" . <123> <hasDigit>
-			 * <123d2> . <123d2> <type> <Digit> . <123d2> <hasPlace> "2" . <123d2>
-			 * <hasActualDigit> "2" . <123> <hasDigit> <123d3> . <123d3> <type> <Digit> .
-			 * <123d3> <hasPlace> "3" . <123d3> <hasActualDigit> "3" .
-			 */
-
 			// first number
 			var b1 = new Binding();
 			b1.put("e", "<e1>");
+			b1.put("hasFirstOrSecondNumber", "<file:///hasFirstNr>");
 			b1.put("n1", "<123>");
 			b1.put("d1", "<123d1>");
 			b1.put("p", "\"1\"");
@@ -112,6 +129,7 @@ public class AdditionForAudienceTest {
 
 			var b2 = new Binding();
 			b2.put("e", "<e1>");
+			b2.put("hasFirstOrSecondNumber", "<file:///hasFirstNr>");
 			b2.put("n1", "<123>");
 			b2.put("d1", "<123d2>");
 			b2.put("p", "\"2\"");
@@ -120,6 +138,7 @@ public class AdditionForAudienceTest {
 
 			var b3 = new Binding();
 			b3.put("e", "<e1>");
+			b3.put("hasFirstOrSecondNumber", "<file:///hasFirstNr>");
 			b3.put("n1", "<123>");
 			b3.put("d1", "<123d3>");
 			b3.put("p", "\"3\"");
@@ -129,6 +148,7 @@ public class AdditionForAudienceTest {
 			// second number
 			var b4 = new Binding();
 			b4.put("e", "<e1>");
+			b4.put("hasFirstOrSecondNumber", "<file:///hasSecondNr>");
 			b4.put("n1", "<456>");
 			b4.put("d1", "<456d1>");
 			b4.put("p", "\"1\"");
@@ -137,6 +157,7 @@ public class AdditionForAudienceTest {
 
 			var b5 = new Binding();
 			b5.put("e", "<e1>");
+			b5.put("hasFirstOrSecondNumber", "<file:///hasSecondNr>");
 			b5.put("n1", "<456>");
 			b5.put("d1", "<456d2>");
 			b5.put("p", "\"2\"");
@@ -145,11 +166,14 @@ public class AdditionForAudienceTest {
 
 			var b6 = new Binding();
 			b6.put("e", "<e1>");
+			b6.put("hasFirstOrSecondNumber", "<file:///hasSecondNr>");
 			b6.put("n1", "<456>");
 			b6.put("d1", "<456d3>");
 			b6.put("p", "\"3\"");
 			b6.put("ad1", "\"6\"");
 			bs.add(b6);
+
+			System.out.println(bs);
 
 			return bs;
 		});
@@ -159,15 +183,16 @@ public class AdditionForAudienceTest {
 		kn.addKB(kbNum);
 
 		this.askKI = new AskKnowledgeInteraction(new CommunicativeAct(), new GraphPattern(
-				"?e <type> <Expression> . ?e <hasOutcome> ?n1 . ?n1 <type> <Number> . ?n1 <hasDigit> ?d1 . ?d1 <hasPlace> ?p . ?d1 <hasActualDigit> ?ad1 ."));
+				"?e <file:///type> <file:///Expression> . ?e <file:///hasOutcome> ?n1 . ?n1 <file:///type> <file:///Number> . ?n1 <file:///hasDigit> ?d1 . ?d1 <file:///hasPlace> ?p . ?d1 <file:///hasActualDigit> ?ad1 ."));
 
 		kbNum.register(askKI);
 
-//		kb10 = new MockedKnowledgeBase("kb10");
-//		kb100 = new MockedKnowledgeBase("kb100");
-
 		kn.sync();
 
+	}
+
+	public String removeFirstLastChar(String withQuotes) {
+		return withQuotes.substring(1, withQuotes.length() - 1);
 	}
 
 	@Test
