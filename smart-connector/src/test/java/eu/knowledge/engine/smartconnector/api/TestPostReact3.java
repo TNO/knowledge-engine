4f0919094571ea1c3cdc6f4b1e31c9cd512cc030
Han Kruiger <han.kruiger@tno.nl>
2023-04-14 13:58:45 +0200

    add unit test for post/react with one normally matching argument and another matching with extra

---
+++ smart-connector/src/test/java/eu/knowledge/engine/smartconnector/api/TestPostReact3.java
@@ -0,0 +1,146 @@
+package eu.knowledge.engine.smartconnector.api;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Iterator;
+
+import org.apache.jena.shared.PrefixMapping;
+import org.apache.jena.sparql.graph.PrefixMappingMem;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class TestPostReact3 {
+	private static MockedKnowledgeBase kb1;
+	private static MockedKnowledgeBase kb2;
+	private static MockedKnowledgeBase kb3;
+
+	private static final Logger LOG = LoggerFactory.getLogger(TestPostReact3.class);
+
+	public boolean kb2Received = false, kb3Received = false;
+
+	@Test
+	public void testPostReact() throws InterruptedException {
+		PrefixMappingMem prefixes = new PrefixMappingMem();
+		prefixes.setNsPrefixes(PrefixMapping.Standard);
+		prefixes.setNsPrefix("ex", "https://example.org/");
+
+		KnowledgeNetwork kn = new KnowledgeNetwork();
+		kb1 = new MockedKnowledgeBase("kb1");
+		kn.addKB(kb1);
+		kb2 = new MockedKnowledgeBase("kb2");
+		kn.addKB(kb2);
+		kb3 = new MockedKnowledgeBase("kb3");
+		kn.addKB(kb3);
+
+		// start registering
+		GraphPattern argumentPattern = new GraphPattern(prefixes, "?a ex:pred1 ?b .");
+		GraphPattern resultPattern1 = new GraphPattern(prefixes, "?c ex:pred2 ?d .");
+		PostKnowledgeInteraction ki1 = new PostKnowledgeInteraction(new CommunicativeAct(), argumentPattern, resultPattern1);
+		kb1.register(ki1);
+
+		ReactKnowledgeInteraction ki2 = new ReactKnowledgeInteraction(new CommunicativeAct(), argumentPattern, resultPattern1);
+		kb2.register(ki2, (anRKI, aReactExchangeInfo) -> {
+			TestPostReact3.this.kb2Received = true;
+
+			var argument = aReactExchangeInfo.getArgumentBindings();
+			Iterator<Binding> iter = argument.iterator();
+			assertTrue(iter.hasNext(), "There should be at least a single binding.");
+			Binding b = iter.next();
+
+			assertEquals("<https://example.org/example/a>", b.get("a"), "Binding of 'a' should be correct.");
+			assertEquals("<https://example.org/example/b>", b.get("b"), "Binding of 'b' should be correct.");
+
+			assertFalse(iter.hasNext(), "This BindingSet should only have a single binding.");
+
+			var bs = new BindingSet();
+			b = new Binding();
+			b.put("c", "<https://example.org/example/c>");
+			b.put("d", "<https://example.org/example/d>");
+			bs.add(b);
+			return bs;
+		});
+
+		GraphPattern resultPattern2 = new GraphPattern(prefixes, "?c ex:pred2 ?d . ?e ex:pred3 ?f .");
+		ReactKnowledgeInteraction ki3 = new ReactKnowledgeInteraction(new CommunicativeAct(), argumentPattern, resultPattern2);
+		kb3.register(ki3, (anRKI, aReactExchangeInfo) -> {
+
+			LOG.info("KB3 Reacting...");
+			TestPostReact3.this.kb3Received = true;
+			var argument = aReactExchangeInfo.getArgumentBindings();
+			Iterator<Binding> iter = argument.iterator();
+			Binding b = iter.next();
+
+			assertEquals("<https://example.org/example/a>", b.get("a"), "Binding of 'a' should be correct.");
+			assertEquals("<https://example.org/example/b>", b.get("b"), "Binding of 'b' should be correct.");
+
+			assertFalse(iter.hasNext(), "This BindingSet should only have a single binding.");
+
+			var bs = new BindingSet();
+			b = new Binding();
+			b.put("c", "<https://example.org/example/c>");
+			b.put("d", "<https://example.org/example/d>");
+			b.put("e", "<https://example.org/example/e>");
+			b.put("f", "<https://example.org/example/f>");
+			bs.add(b);
+			return bs;
+		});
+
+		kn.sync();
+		LOG.info("Everyone is up-to-date!");
+
+		// start exchanging
+		BindingSet bindingSet = new BindingSet();
+		Binding binding = new Binding();
+		binding.put("a", "<https://example.org/example/a>");
+		binding.put("b", "<https://example.org/example/b>");
+		bindingSet.add(binding);
+
+		try {
+			PostResult result = kb1.post(ki1, bindingSet).get();
+
+			assertTrue(this.kb2Received, "KB2 should have received the posted data.");
+			assertTrue(this.kb3Received, "KB3 should have received the posted data.");
+			BindingSet bs = result.getBindings();
+			LOG.info("received post results: {}", bs);
+			assertTrue(bs.size() > 0);
+			bs.forEach(b -> {
+				assertEquals(new HashSet<String>(Arrays.asList("c", "d")), b.getVariables());
+			});
+		} catch (Exception e) {
+			LOG.error("Error", e);
+		}
+	}
+
+	@AfterAll
+	public static void cleanup() {
+		LOG.info("Clean up: {}", TestPostReact3.class.getSimpleName());
+		if (kb1 != null) {
+			kb1.stop();
+		} else {
+			fail("KB1 should not be null!");
+		}
+
+		if (kb2 != null) {
+
+			kb2.stop();
+		} else {
+			fail("KB2 should not be null!");
+		}
+
+		if (kb3 != null) {
+
+			kb3.stop();
+		} else {
+			fail("KB3 should not be null!");
+		}
+	}
+
+}
