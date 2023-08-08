3b789b450a5c4b36e2307541fb6cd09e22a4c7f3
Barry Nouwt <barry.nouwt@tno.nl>
2023-08-08 16:07:54 +0200

    Improved javadocs and added FAQ.

--- smart-connector-api/src/main/java/eu/knowledge/engine/smartconnector/api/AnswerHandler.java
+++ smart-connector-api/src/main/java/eu/knowledge/engine/smartconnector/api/AnswerHandler.java
@@ -10,24 +10,27 @@ import java.util.concurrent.CompletableFuture;
 public interface AnswerHandler {
 
 	/**
-	 * Asynchronous handler for this interaction. The default implementation
-	 * simply blocks and calls the synchronous method, but by overriding this
-	 * default implementation, you can use an event-based architecture.
+	 * Asynchronous handler for this interaction. The default implementation simply
+	 * blocks and calls the synchronous method, but by overriding this default
+	 * implementation, you can use an event-based architecture.
 	 * 
-	 * @param anAKI The {@link KnowledgeInteraction} that is involved in the
-	 *              question top answer.
+	 * @param anAKI                The {@link KnowledgeInteraction} that is involved
+	 *                             in the question top answer.
 	 * @param anAnswerExchangeInfo An {@link AnswerExchangeInfo}, containing a set
-	 *   of {@link Binding}s for variables in the {@link KnowledgeInteraction}'s
-	 *   {@link GraphPattern}, and more information such as the ID of the ASKing
-	 *   knowledge base.
+	 *                             of {@link Binding}s for variables in the
+	 *                             {@link KnowledgeInteraction}'s
+	 *                             {@link GraphPattern}, and more information such
+	 *                             as the ID of the ASKing knowledge base.
 	 * @return A future of all {@link Binding}s that match this
-	 *   {@link KnowledgeInteraction}'s {@link GraphPattern}, AND matches ANY of
-	 *   the input BindingSet. All variables from the {@link GraphPattern} are
-	 *   bound, including the ones that were already provided in the input
-	 *   {@link BindingSet}. If not, you would not be able to know which partial
-	 *   binding output corresponded with which partial binding input.
+	 *         {@link KnowledgeInteraction}'s {@link GraphPattern}, AND matches ANY
+	 *         of the input BindingSet. All variables from the {@link GraphPattern}
+	 *         are bound, including the ones that were already provided in the input
+	 *         {@link BindingSet}. If not, you would not be able to know which
+	 *         partial binding output corresponded with which partial binding input.
+	 *         Should not return null.
 	 */
-	public default CompletableFuture<BindingSet> answerAsync(AnswerKnowledgeInteraction anAKI, AnswerExchangeInfo anAnswerExchangeInfo) {
+	public default CompletableFuture<BindingSet> answerAsync(AnswerKnowledgeInteraction anAKI,
+			AnswerExchangeInfo anAnswerExchangeInfo) {
 		CompletableFuture<BindingSet> future = new CompletableFuture<BindingSet>();
 		try {
 			BindingSet bs = this.answer(anAKI, anAnswerExchangeInfo);
@@ -39,18 +42,20 @@ public interface AnswerHandler {
 	}
 
 	/**
-	 * @param anAKI       The {@link KnowledgeInteraction} that is involved in the
-	 *                    question top answer.
-	 * @param anAnswerExchangeInfo
-	 *   An {@link AnswerExchangeInfo}, containing a set of {@link Binding}s for
-	 *   variables in the {@link KnowledgeInteraction}'s {@link GraphPattern}, and
-	 *   more information such as the ID of the ASKing knowledge base.
+	 * @param anAKI                The {@link KnowledgeInteraction} that is involved
+	 *                             in the question to answer.
+	 * @param anAnswerExchangeInfo An {@link AnswerExchangeInfo}, containing a set
+	 *                             of {@link Binding}s for variables in the
+	 *                             {@link KnowledgeInteraction}'s
+	 *                             {@link GraphPattern}, and more information such
+	 *                             as the ID of the ASKing knowledge base.
 	 * @return All {@link Binding}s that match this {@link KnowledgeInteraction}'s
 	 *         {@link GraphPattern}, AND matches ANY of the input BindingSet. All
 	 *         variables from the {@link GraphPattern} are bound, including the ones
 	 *         that were already provided in the input {@link BindingSet}. If not,
 	 *         you would not be able to know which partial binding output
-	 *         corresponded with which partial binding input.
+	 *         corresponded with which partial binding input. Should not return
+	 *         null.
 	 */
 	public abstract BindingSet answer(AnswerKnowledgeInteraction anAKI, AnswerExchangeInfo anAnswerExchangeInfo);
 }
