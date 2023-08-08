df507d6a03d1e90ece6fb8d321dfcec930007961
Han Kruiger <han.kruiger@tno.nl>
2023-08-08 14:10:01 +0200

    KE HTTP API: When deleting smart connector fails, don't remove it from the list, and log.

--- smart-connector-rest-server/src/main/java/eu/knowledge/engine/rest/api/impl/RestKnowledgeBaseManager.java
+++ smart-connector-rest-server/src/main/java/eu/knowledge/engine/rest/api/impl/RestKnowledgeBaseManager.java
@@ -121,11 +121,17 @@ public class RestKnowledgeBaseManager {
 			if (rkb != null)
 				rkb.stop();
 			success = true;
-		} catch (IllegalStateException e) {
+		} catch (RuntimeException e) {
+			LOG.error("Could not stop knowledge base {}, so it stays in the list of knowledge bases", knowledgeBaseId);
+			LOG.error("Encountered exception while stopping knowledge base", e);
 			success = false;
 		}
-		this.restKnowledgeBases.remove(knowledgeBaseId);
-		LOG.info("Removed KB {}", knowledgeBaseId);
+
+		if (success) {
+			this.restKnowledgeBases.remove(knowledgeBaseId);
+			LOG.info("Removed KB {}", knowledgeBaseId);
+		}
+
 		return success;
 	}
 
